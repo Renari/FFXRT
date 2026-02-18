86ec324d717ecf72fb82f4afc29c3045669a3ec6
Arimil <renari@arimil.com>
2026-02-18 16:18:21 -0500

    set desktop file name (required for wayland icon)

--- FFXRT/main.cpp
+++ FFXRT/main.cpp
@@ -5,6 +5,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setDesktopFileName("ffxrt");
     a.setWindowIcon(QIcon(":/ffxrt.png"));
     MainWindow w;
     w.show();
